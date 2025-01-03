e67ef8b97e7600e6728e4a5c38cb6d4f6304f627
Kiran Jikuru <kiranjikuru990@gmail.com>
2025-01-03 12:26:04 +0530

    Update Kiran.java

--- Kiran.java
+++ Kiran.java
@@ -2,6 +2,7 @@ public class Kiran
 {
     public static void main(String args[]){
 
-        System.out.println("enter your name");
+      String ary[] = {"harish", "kiran", "Charan"};
+        System.out.print(ary.length);
     }
 }
